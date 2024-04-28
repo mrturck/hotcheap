3ee980841e801b4975826b1d49d05643a935ef9f
Rory Hughes <544628+rorhug@users.noreply.github.com>
2024-04-28 11:53:24 +0100

    check geo

--- src/pages/api/geo.ts
+++ src/pages/api/geo.ts
@@ -1,20 +1,21 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
+import type { NextApiRequest, NextApiResponse } from "next"
 
 interface ResponseData {
-    message: string;
+  message?: string
+  headers?: unknown
 }
 
 export default function handler(
-    req: NextApiRequest,
-    res: NextApiResponse<ResponseData>
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseData>,
 ) {
-    if (req.method === 'GET') {
-        const city = req.headers["X-Vercel-IP-City"] ?? "unknown";
-        res.status(200).json({ message: `Hello ${city as string}` });
-    } else {
-        res.setHeader('Allow', ['GET']);
-        res.status(405).json({ message: `Method ${req.method} not allowed` });
-    }
+  if (req.method === "GET") {
+    const headers = req.headers
+    res.status(200).json({ headers })
+  } else {
+    res.setHeader("Allow", ["GET"])
+    res.status(405).json({ message: `Method ${req.method} not allowed` })
+  }
 }
 
-export { handler as GET, handler as POST };
+export { handler as GET, handler as POST }
