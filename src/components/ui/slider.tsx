e746b8e502dfcb50acfb45708a95ccc70cc5487c
Michael Turck <michaelrturck@gmail.com>
2024-06-03 20:37:43 +0100

    fix compile

--- src/components/ui/slider.tsx
+++ src/components/ui/slider.tsx
@@ -8,23 +8,31 @@ import { cn } from "~/lib/utils"
 const Slider = React.forwardRef<
   React.ElementRef<typeof SliderPrimitive.Root>,
   React.ComponentPropsWithoutRef<typeof SliderPrimitive.Root>
->(({ className, ...props }, ref) => (
-  <SliderPrimitive.Root
-    ref={ref}
-    className={cn(
-      "relative flex w-full touch-none select-none items-center",
-      className,
-    )}
-    {...props}
-  >
-    <SliderPrimitive.Track className="relative h-3 w-full grow overflow-hidden rounded-full bg-white/20">
-      <SliderPrimitive.Range className="absolute h-full bg-white" />
-    </SliderPrimitive.Track>
-    <SliderPrimitive.Thumb className=" -mt-3 block h-6 w-7 rounded-full text-3xl transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50">
-      {props?.value == 0.5 ? "😎" : props?.value < 0.5 ? "🤑" : "🥵"}
-    </SliderPrimitive.Thumb>
-  </SliderPrimitive.Root>
-))
+>(({ className, ...props }, ref) => {
+  const emoji = React.useMemo(() => {
+    if (!props.value || !props.value[0] || props?.value[0] == 0.5) return "😎"
+    if (props.value?.[0] < 0.5) return "🤑"
+    return "🥵"
+  }, [props.value])
+
+  return (
+    <SliderPrimitive.Root
+      ref={ref}
+      className={cn(
+        "relative flex w-full touch-none select-none items-center",
+        className,
+      )}
+      {...props}
+    >
+      <SliderPrimitive.Track className="relative h-3 w-full grow overflow-hidden rounded-full bg-white/20">
+        <SliderPrimitive.Range className="absolute h-full bg-white" />
+      </SliderPrimitive.Track>
+      <SliderPrimitive.Thumb className=" -mt-3 block h-6 w-7 rounded-full text-3xl transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50">
+        {emoji}
+      </SliderPrimitive.Thumb>
+    </SliderPrimitive.Root>
+  )
+})
 Slider.displayName = SliderPrimitive.Root.displayName
 
 export { Slider }
